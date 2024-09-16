49663bb4e23f07ad2c62f45f2c0c7f6f8f86c992
Rowan McAlpin <87948250+rowan-mcalpin@users.noreply.github.com>
2024-09-15 21:51:56 -0700

    Version bump 0.3.0

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -93,7 +93,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.rowanmcalpin.nextftc"
             artifactId = "core"
-            version = "0.2.2"
+            version = "0.3.0"
 
             afterEvaluate {
                 from(components["release"])
