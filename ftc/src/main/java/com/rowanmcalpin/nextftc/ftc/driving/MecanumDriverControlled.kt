8f340498a7431e91e533d34142efecf75afd2f26
rowan-mcalpin <87948250+rowan-mcalpin@users.noreply.github.com>
2025-01-13 14:34:23 -0800

    Repair MecanumDriverControlled

--- ftc/src/main/java/com/rowanmcalpin/nextftc/ftc/driving/MecanumDriverControlled.kt
+++ ftc/src/main/java/com/rowanmcalpin/nextftc/ftc/driving/MecanumDriverControlled.kt
@@ -40,31 +40,37 @@ import kotlin.math.sin
  * @param turnJoystick: The joystick to use for turning
  * @param robotCentric: Whether to use robot centric or field centric movement
  */
-
-class MecanumDriverControlled(val motors: Array<Controllable>, val driveJoystick: Joystick, val turnJoystick: Joystick, val robotCentric: Boolean, val imu: IMU?): Command() {
+class MecanumDriverControlled(val motors: Array<out Controllable>, val driveJoystick: Joystick, val turnJoystick: Joystick, val robotCentric: Boolean, val imu: IMU?): Command() {
     /**
      * @param motors: An array of your drive motors in the following order: left front, right front, left back, right back
      * @param driveJoystick: The joystick to use for forward and strafe movement
      * @param turnJoystick: The joystick to use for turning
      */
-    constructor(motors: Array<Controllable>, driveJoystick: Joystick, turnJoystick: Joystick): this(motors, driveJoystick, turnJoystick, true, null)
+    constructor(motors: Array<out Controllable>, driveJoystick: Joystick, turnJoystick: Joystick): this(motors, driveJoystick, turnJoystick, true, null)
 
     /**
      * @param motors: An array of your drive motors in the following order: left front, right front, left back, right back
      * @param gamepad: The gamepad to use the joysticks from
      * @param robotCentric: Whether to use robot centric or field centric movement
      */
-    constructor(motors: Array<Controllable>, gamepad: GamepadEx, robotCentric: Boolean, imu: IMU): this(motors, gamepad.leftStick, gamepad.rightStick, robotCentric, imu)
+    constructor(motors: Array<out Controllable>, gamepad: GamepadEx, robotCentric: Boolean, imu: IMU): this(motors, gamepad.leftStick, gamepad.rightStick, robotCentric, imu)
 
     /**
      * @param motors: An array of your drive motors in the following order: left front, right front, left back, right back
      * @param gamepad: The gamepad to use the joysticks from
      */
-    constructor(motors: Array<Controllable>, gamepad: GamepadEx): this(motors, gamepad.leftStick, gamepad.rightStick, true, null)
+    constructor(motors: Array<out Controllable>, gamepad: GamepadEx): this(motors, gamepad.leftStick, gamepad.rightStick, true, null)
 
     override val isDone: Boolean = false
 
     override val subsystems: Set<Subsystem> = setOf(Drivetrain)
+
+    var scalar = 1.0
+    private var offset: Double = 0.0
+
+    var orientation: Double
+        get() = imu!!.robotYawPitchRollAngles.getYaw(AngleUnit.RADIANS) - offset
+        set(value) { offset = imu!!.robotYawPitchRollAngles.getYaw(AngleUnit.RADIANS) - value }
 
     override fun start() {
         if (motors.size != 4) throw IllegalArgumentException("motors must be an array of 4 motors.")
@@ -72,35 +78,43 @@ class MecanumDriverControlled(val motors: Array<Controllable>, val driveJoystick
     }
 
     override fun update() {
-        var y = -gamepad1.left_stick_y.toDouble()
-        var x = gamepad1.left_stick_x.toDouble()
-        val rx = gamepad1.right_stick_x.toDouble()
+        var y = driveJoystick.y.toDouble()
+        var x = driveJoystick.x.toDouble()
+        val rx = turnJoystick.x.toDouble()
 
         if (!robotCentric) {
-            val botHeading = imu!!.robotYawPitchRollAngles.getYaw(AngleUnit.RADIANS)
-
+            val botHeading = orientation
 
             // Rotate the movement direction counter to the bot's rotation
-            x = x * cos(-botHeading) - y * sin(-botHeading)
-            y = x * sin(-botHeading) + y * cos(-botHeading)
+            var rotX = x * cos(-botHeading) - y * sin(-botHeading)
+            val rotY = x * sin(-botHeading) + y * cos(-botHeading)
+
+            rotX *= 1.1 // Counteract imperfect strafing
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            val denominator = max(abs(rotY) + abs(rotX) + abs(rx), 1.0)
+            val frontLeftPower = (rotY + rotX + rx) / denominator
+            val backLeftPower = (rotY - rotX + rx) / denominator
+            val frontRightPower = (rotY - rotX - rx) / denominator
+            val backRightPower = (rotY + rotX - rx) / denominator
+
+            motors[0].power = (frontLeftPower * scalar)
+            motors[1].power = (frontRightPower * scalar)
+            motors[2].power = (backLeftPower * scalar)
+            motors[3].power = (backRightPower * scalar)
+        } else {
+            val denominator = max(abs(y) + abs(x) + abs(rx), 1.0)
+            val frontLeftPower = (y + x + rx) / denominator
+            val backLeftPower = (y - x + rx) / denominator
+            val frontRightPower = (y - x - rx) / denominator
+            val backRightPower = (y + x - rx) / denominator
+
+            motors[0].power = (frontLeftPower)
+            motors[1].power = (frontRightPower)
+            motors[2].power = (backLeftPower)
+            motors[3].power = (backRightPower)
         }
-
-
-        x *= 1.1 // Counteract imperfect strafing
-
-
-        // Denominator is the largest motor power (absolute value) or 1
-        // This ensures all the powers maintain the same ratio,
-        // but only if at least one is out of the range [-1, 1]
-        val denominator = max(abs(y) + abs(x) + abs(rx), 1.0)
-        val frontLeftPower = (y + x + rx) / denominator
-        val backLeftPower = (y - x + rx) / denominator
-        val frontRightPower = (y - x - rx) / denominator
-        val backRightPower = (y + x - rx) / denominator
-
-        motors[0].power = frontLeftPower
-        motors[1].power = frontRightPower
-        motors[2].power = backLeftPower
-        motors[3].power = backRightPower
     }
 }
