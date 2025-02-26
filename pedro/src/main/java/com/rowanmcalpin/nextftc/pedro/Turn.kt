fa52efe93c2785c1df4304f9aecc59b531248981
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2025-02-26 13:57:29 -0600

    Fix Turn and TurnTO

--- pedro/src/main/java/com/rowanmcalpin/nextftc/pedro/Turn.kt
+++ pedro/src/main/java/com/rowanmcalpin/nextftc/pedro/Turn.kt
@@ -11,16 +11,16 @@ import com.rowanmcalpin.nextftc.core.units.deg
  * @param tolerance the maximum error between the current heading and the target heading required to finish
  * @throws FollowerNotInitializedException if the follower is not set
  */
-class Turn(private val angle: Angle, private val tolerance: Angle = 5.deg): Command() {
+class Turn @JvmOverloads constructor(private val angle: Angle, private val tolerance: Angle = 5.deg): Command() {
     private lateinit var targetHeading: Angle
 
     override val isDone: Boolean
-        get() = abs(targetHeading.normalized - Angle(PedroData.follower!!.totalHeading).normalized) <= tolerance.abs
+        get() = abs(targetHeading.normalized - Angle.fromRad(PedroData.follower!!.totalHeading).normalized) <= tolerance.abs
 
     override fun start() {
         if (PedroData.follower == null) throw FollowerNotInitializedException()
 
-        targetHeading = Angle(PedroData.follower!!.totalHeading) + angle
+        targetHeading = Angle.fromRad(PedroData.follower!!.totalHeading) + angle
         PedroData.follower!!.turn(angle.abs.inRad, angle.sign > 0)
     }
 }
--- pedro/src/main/java/com/rowanmcalpin/nextftc/pedro/TurnTo.kt
+++ pedro/src/main/java/com/rowanmcalpin/nextftc/pedro/TurnTo.kt
@@ -11,9 +11,9 @@ import com.rowanmcalpin.nextftc.core.units.deg
  * @param tolerance the maximum error between the current heading and the target heading required to finish
  * @throws FollowerNotInitializedException if the follower is not set
  */
-class TurnTo(private val targetHeading: Angle, private val tolerance: Angle = 5.deg): Command() {
+class TurnTo @JvmOverloads constructor(private val targetHeading: Angle, private val tolerance: Angle = 5.deg): Command() {
     override val isDone: Boolean
-        get() = abs(targetHeading.normalized - Angle(PedroData.follower!!.totalHeading).normalized) <= tolerance.abs
+        get() = abs(targetHeading.normalized - Angle.fromRad(PedroData.follower!!.totalHeading).normalized) <= tolerance.abs
 
     override fun start() {
         if (PedroData.follower == null) throw FollowerNotInitializedException()
